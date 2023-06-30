f556a1c64f418977ede90438e228d5abcb6e64b0
Niko <nikitafander@gmail.com>
2023-07-01 02:12:10 +0400

    modified:   freedownloadmanager.sh

--- freedownloadmanager.sh
+++ freedownloadmanager.sh
@@ -2,7 +2,7 @@
 
 echo "Downloading dependencies..."
 
-wget -c -nv https://github.com/FanderWasTaken/freedownloadmanager-linux-appimage/main/freedownloadmanager.yml
+wget -c -nv https://raw.githubusercontent.com/FanderWasTaken/freedownloadmanager-linux-appimage/main/freedownloadmanager.yml
 wait
 wget -c -nv https://github.com/AppImageCommunity/pkg2appimage/releases/download/continuous/pkg2appimage-1807-x86_64.AppImage
 wait
